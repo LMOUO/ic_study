7302f0768a119cf94ec7ce8db38c5ca9fd5b2ffc
LMOUO <104372363+LMOUO@users.noreply.github.com>
2026-07-06 09:05:29 +0000

    更新代码

--- project/syn/script.tcl
+++ project/syn/script.tcl
@@ -1,4 +1,24 @@
+# 1. 读入设计
 read_verilog ../rtl/led_test.v
+
+# 2. 设置顶层
 synth -top led_test
+
+# 3. 常规优化（工艺无关）
+proc; opt
+
+# 4. 映射到 Nangate 标准单元
+#    将 RTL 中的寄存器、组合逻辑映射到库的具体单元
+#techmap -map /dev/null                      # 清空内置映射
+#dfflibmap -liberty ../libs/Nangate45/NangateOpenCellLibrary_typical.lib
+# abc -liberty ../libs/Nangate45/NangateOpenCellLibrary_typical.lib
+
+# 5. 清理与优化
+opt
+
+# 6. 写出门级网表
 write_verilog led_test_netlist.v
+
+# 7. 输出带真实面积的报告
+#stat -liberty ../libs/Nangate45/NangateOpenCellLibrary_typical.lib
 stat
