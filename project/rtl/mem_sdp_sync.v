f1e377c1e2e30cdc74d844f7fb167d2462b64ad9
LMOUO <104372363+LMOUO@users.noreply.github.com>
2026-07-11 02:38:00 +0000

    更新代码

--- project/rtl/mem_sdp_sync.v
+++ project/rtl/mem_sdp_sync.v
@@ -14,17 +14,75 @@ module mem_sdp_sync
 
     input  wire                     rd_en,
     input  wire [ADDR_WIDTH-1:0]    rd_addr,
-    output reg  [DATA_WIDTH-1:0]    rd_data
+    output wire [DATA_WIDTH-1:0]    rd_data
 );
 
-reg [DATA_WIDTH-1:0] mem [0:MEM_DEPTH-1];
+`ifdef USE_SRAM_MACRO
 
+    /*
+     * SKY130 SRAM宏：  
+     * 8 bit × 1024 word
+     *
+     * 当前FIFO深度128，只使用地址0～127。
+     */
+    localparam SRAM_ADDR_WIDTH = 10;
+
+    wire [SRAM_ADDR_WIDTH-1:0] sram_wr_addr;
+    wire [SRAM_ADDR_WIDTH-1:0] sram_rd_addr;
+    wire [7:0] unused_dout0;
+
+    /*
+     * 仅允许：
+     * DATA_WIDTH = 8
+     * MEM_DEPTH <= 1024
+     */
+    assign sram_wr_addr = {{(SRAM_ADDR_WIDTH-ADDR_WIDTH){1'b0}},
+                           wr_addr};
+
+    assign sram_rd_addr = {{(SRAM_ADDR_WIDTH-ADDR_WIDTH){1'b0}},
+                           rd_addr};
+
+    sky130_sram_1kbyte_1rw1r_8x1024_8 #(
+        .VERBOSE (0)
+    ) u_sram (
+        /*
+         * Port 0：1RW端口
+         * 在FIFO中固定作为写端口。
+         */
+        .clk0   (clk),
+        .csb0   (~wr_en),
+        .web0   (1'b0),
+        .wmask0 (1'b1),
+        .addr0  (sram_wr_addr),
+        .din0   (wr_data),
+        .dout0  (unused_dout0),
+
+        /*
+         * Port 1：只读端口
+         */
+        .clk1   (clk),
+        .csb1   (~rd_en),
+        .addr1  (sram_rd_addr),
+        .dout1  (rd_data)
+    );
+
+`else
+    /*
+     * 通用RTL行为模型。
+     */
+
+reg [DATA_WIDTH-1:0] mem [0:MEM_DEPTH-1];
+reg [DATA_WIDTH-1:0] rd_data_reg;
 always @(posedge clk) begin
     if(wr_en)
         mem[wr_addr] <= wr_data;
 
     if(rd_en)
-        rd_data <= mem[rd_addr];
+        rd_data_reg <= mem[rd_addr];
 end
+
+assign  rd_data = rd_data_reg;
+
+`endif
 
 endmodule
